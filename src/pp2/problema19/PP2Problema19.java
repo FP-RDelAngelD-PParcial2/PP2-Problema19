c7e3ade0ce2f248eeb8a8fce78097b67606981a2
PC <PC@HP>
2016-10-17 19:30:07 -0500

    19.	Realiza un programa que, dado el valor de un ángulo, calcule su seno, coseno y tangente.

--- src/pp2/problema19/PP2Problema19.java
+++ src/pp2/problema19/PP2Problema19.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pp2.problema19;
-
+import java.util.*;
 /**
  *
  * @author PC
@@ -17,5 +17,19 @@ public class PP2Problema19 {
     public static void main(String[] args) {
         
     }
-    
+     public static int solicitarDatos(String d){ //Solicita los valores
+        int miDato;
+        Scanner entradaEscaner = new Scanner(System.in);
+        System.out.print("Introduce el dato " + d);
+        miDato= entradaEscaner.nextInt();
+        return miDato;
+    }
+    public static int verificarNumero(String d){
+        int a; //Variable
+        do{ //Hacer que solicite datos y se guarden en a 
+        a = solicitarDatos(d);
+        }while(a<=0);//Mientras que a sea menor o igual que 0
+        return a; //Regresa el valor de a
+    }
+    public static
 }
