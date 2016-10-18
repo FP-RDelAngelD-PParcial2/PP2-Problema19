a0044a3101d0d6a23ef5d951539e7256991ddefc
PC <PC@HP>
2016-10-17 20:10:11 -0500

    19.	Realiza un programa que, dado el valor de un ángulo, calcule su seno, coseno y tangente.

--- src/pp2/problema19/PP2Problema19.java
+++ src/pp2/problema19/PP2Problema19.java
@@ -15,9 +15,39 @@ public class PP2Problema19 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        verificarAngulo("1");
+        funcionesT();
     }
-     public static double solicitarDatos(String d){ //Solicita los valores
+    
+    public static void calcularAngulo(){ //Calcula cada una de las funciones
+        switch(menu()){ //Entra la opcion elegida en el metodo pasado
+            case 1:
+                convertirSeno();
+                break;
+            case 2:
+                convertirCoseno();
+                break;
+            case 3:
+                convertirTangente();
+                break;
+            case 4:
+                funcionesT();
+                break;
+            }
+    }
+    public static int menu(){ //Creacion de un menu para dar al usuario a elegir la opcion deseada
+        int opcion;
+        Scanner teclado = new Scanner (System.in);
+        do{
+            System.out.print("Introduce la opcion deseada:" + "\n");
+            System.out.print("1. Seno" + "\n");
+            System.out.print("2. Coseno" + "\n");
+            System.out.print("3. Tangente" + "\n");
+            System.out.print("4. Todas" + "\n");
+            opcion=teclado.nextInt();
+        }while (opcion <=0 || opcion >4); //Mostrar el menu mientras que la opcion elegida sea menor o igual que 0 o mayor que 4
+        return opcion;
+    }
+    public static double solicitarDatos(String d){ //Solicita los valores
         double miDato;
         Scanner entradaEscaner = new Scanner(System.in);
         System.out.print("Introduce el dato " + d);
@@ -31,4 +61,14 @@ public class PP2Problema19 {
         }while(a<0 || a>360);//Mientras que a sea menor que 0 o a sea mayor a 360
         return a;     
     }
+    public static void funcionesT(){
+        double a, sen, cos, tan;
+        a = verificarAngulo("angulo del cual se quiere obtener seno, coseno y tangente");
+        a = Math.toRadians(a);
+        sen=Math.sin(a);
+        cos=Math.cos(a);
+        tan=Math.tan(a);
+        System.out.print(sen +"+"+ cos +"+"+ tan);    
+    }
+    
 }
